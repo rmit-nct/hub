e05d6ab1f80b023d49832cb029bf2e2458113ad2
phatgg221 <129394719+phatgg221@users.noreply.github.com>
2024-06-10 22:15:48 +0700

    update: to-do list setup

---
+++ apps/web/src/app/[lang]/(dashboard)/[wsId]/finance/budgetPlanning/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>budget planning!</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
+
--- apps/web/src/components/finance_component/toDoCard.tsx
+++ apps/web/src/components/finance_component/toDoCard.tsx
@@ -2,29 +2,31 @@ import React from 'react';
 
 const ToDoTasks = () => {
   return (
-      <div className="text-center w-[800px] bg-gray-800 p-6 rounded-3xl inline-block relative">
-        <h2 className="text-3xl font-bold mb-4 flex items-center justify-left">
-          To-do tasks: <span className="ml-2 ">🎯</span>
-        </h2>
-        <ul className="ml-[50px] text-left">
-          <li className="mt-[70px] mb-2">
-            <button className="text-white font-bold line-through w-full text-left text-2xl">
-              Member fee tracking.
-            </button>
-          </li>
-          <li className="mt-[70px] mb-2">
-            <button className="text-white font-bold w-full text-left text-2xl">
-              Bill tracking.
-            </button>
-          </li>
-          <li className='mt-[70px]'>
-            <button className="text-white font-bold w-full text-left text-2xl">
-              Budget planning.
-            </button>
-          </li>
-        </ul>
-       
-      </div>
+    <div className="relative inline-block w-[800px] rounded-3xl bg-gray-800 p-6 text-center">
+      <h2 className="justify-left mb-4 flex items-center text-3xl font-bold">
+        To-do tasks: <span className="ml-2 ">🎯</span>
+      </h2>
+      <ul className="ml-[50px] text-left">
+        <li className="mb-2 mt-[70px]">
+          <a href={'/finance/memberFeeTracking'} className="w-full text-left text-2xl font-bold text-white line-through">
+            Member fee tracking.
+          </a>
+        </li>
+        <li className="mb-2 mt-[70px]">
+          <a
+            href={'/finance/billTracking'}
+            className="w-full text-left text-2xl font-bold text-white"
+          >
+            Bill tracking.
+          </a>
+        </li>
+        <li className="mt-[70px]">
+          <a href={'/finance/budgetPlanning'} className="w-full text-left text-2xl font-bold text-white">
+            Budget planning.
+          </a>
+        </li>
+      </ul>
+    </div>
   );
 };
 
