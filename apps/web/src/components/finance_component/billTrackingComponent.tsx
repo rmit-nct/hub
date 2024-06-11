98c498f3105b467084da21bd039e3d2dc915317c
phatgg221 <129394719+phatgg221@users.noreply.github.com>
2024-06-11 20:30:01 +0700

    feat: bill tracking setup

---
+++ apps/web/src/components/finance_component/billTrackingComponent.tsx
@@ -0,0 +1,57 @@
+"use client"
+import React,{useState} from "react";
+import Modal from "../forms/billTrackingForm";
+import { User } from "lucide-react";
+
+
+interface BillDetails {
+    billName: string;
+    buyingDate: string;
+    billCreatedDate: string;
+    itemList: string[];
+    memberInCharge: string;
+    redBillStatus: string;
+    redBillMessage: string;
+    whiteBillURL: string;
+    totalPrice: string;
+    paidAmount: string;
+    note: string;
+  }
+  const [showModal, setShowModal]= useState(false);
+  const [selectedBills, setSelectedBills]= useState<BillDetails | null> (null);
+  const [selected, setSelected] = useState(false);
+  const handleClick= (bills: BillDetails)=>{
+    setSelectedBills(bills);
+    setShowModal(true);
+    setSelected(true);
+  }
+
+  const closeModal= ()=>{
+    setShowModal(false);
+  };
+
+  const billDataTable: React.FC =() =>{
+    const billDetails: BillDetails = {
+        billName: "The form would maybe look like",
+        buyingDate: "18/06/2024",
+        billCreatedDate: "20/06/2024",
+        itemList: ["Item list:", "Item list:", "Item list:", "Item list:"],
+        memberInCharge: "Marcus Martin",
+        redBillStatus: "Red bill has not been provided.",
+        redBillMessage: "Click here to send email to member that is in charge.",
+        whiteBillURL: "https://example.com/images/white_bill.png",
+        totalPrice: "",
+        paidAmount: "",
+        note: "",
+      };
+
+      return (
+        <div>
+            <a onClick={() => handleClick(billDetails)}>Hello click me</a>
+            
+        </div>
+      );
+
+  }
+
+  export default billDataTable;
---
+++ apps/web/src/components/forms/billTrackingForm.tsx
@@ -0,0 +1,41 @@
+import React,{useState} from "react";
+import Image from "next/image";
+import ImageModal from "../picture_zoom";
+
+interface BillDetails {
+    billName: string;
+    buyingDate: string;
+    billCreatedDate: string;
+    itemList: string[];
+    memberInCharge: string;
+    redBillStatus: string;
+    redBillMessage: string;
+    whiteBillURL: string;
+    totalPrice: string;
+    paidAmount: string;
+    note: string;
+  }
+
+  interface ModalProps{
+    show: boolean;
+    billDetail: BillDetails;
+    onClose: ()=> void;
+  }
+
+  const Modal: React.FC<ModalProps> =({show, billDetail, onClose})=>{
+    const [showModal, setShowModal]= useState(false);
+    
+    const closeModal=()=>{
+        setShowModal(false);
+    }
+
+    if(!show) return null;
+
+    return(
+        <div className="fixed inset-0 bg-gray-900 bg-opacity-75 flex justify-center items-center">
+            
+        </div>
+    );
+  }
+
+  export default Modal;
