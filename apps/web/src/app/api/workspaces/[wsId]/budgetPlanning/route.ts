603c2ff2dc1a10cc47e93133285a1c96be0e20c7
phatgg221 <129394719+phatgg221@users.noreply.github.com>
2024-06-18 16:07:13 +0700

    update: api set-up for budget planning

---
+++ apps/web/src/app/api/workspaces/[wsId]/budgetPlanning/route.ts
@@ -0,0 +1,32 @@
+import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
+import {cookies} from 'next/headers';
+import { NextRequest, NextResponse } from 'next/server';
+
+export const dynamic= 'force-dynamic';
+
+
+interface Params{
+    params:{
+        wsId: string;
+    };
+}
+
+
+export async function POST(req: NextRequest, { params: {wsId :id} }: Params) {
+    const supabase = createServerComponentClient({ cookies });
+    // const { wsId } = params;
+    console.log(id);
+    const { taskId, updates } = await req.json();
+  
+    const { data, error } = await supabase
+      .from('budget_planning')
+      .update(updates)
+      .eq('id', taskId)
+  
+    if (error) {
+      return NextResponse.json({ error: error.message }, { status: 500 });
+    }
+  
+    return NextResponse.json({ data });
+  }
+  
