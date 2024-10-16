a344a6a66a1ce0c8b76e4de55eea22a24da5be1f
Nguyen Khang <nguyengiakhang646@gmail.com>
2024-10-16 11:04:34 +0700

    make this branch deployable

--- apps/web/src/app/[locale]/(marketing)/page.tsx
+++ apps/web/src/app/[locale]/(marketing)/page.tsx
@@ -1,15 +1,4 @@
 import AboutUs from './about';
-import GetStartedButton from './get-started-button';
-import Members from './members';
-import {
-  CalendarHeart,
-  Cpu,
-  MemoryStick,
-  Network,
-  Sparkles,
-  Telescope,
-} from 'lucide-react';
-import { useEffect } from 'react';
 
 export default function MarketingPage() {
   return (
@@ -22,7 +11,6 @@ export default function MarketingPage() {
           <div className="col-span-2 h-2/3 rounded-xl bg-black/20"></div>
           <div className="col-span-2 h-2/3 rounded-xl bg-black/20"></div>
         </div>
-        
       </div>
       <div className="absolute inset-0 z-10 flex h-screen items-center justify-center">
         <div className="aspect-square h-32 w-32 rounded-full bg-[radial-gradient(circle,_rgba(217,180,48,0.8)_0%,_rgba(113,113,122,0.1)_80%)] md:h-64 md:w-64 lg:h-[36rem] lg:w-[36rem]"></div>
