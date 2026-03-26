cec6a085ec7550a74cd0d5931c97854445b81a8b
dat-nix <dp2838400@gmail.com>
2026-03-26 10:22:03 +0700

    test

--- apps/web/src/app/api/v1/calendar/auth/fetch/route.ts
+++ apps/web/src/app/api/v1/calendar/auth/fetch/route.ts
@@ -1,5 +1,4 @@
 import { createClient } from '@ncthub/supabase/next/server';
-import { OAuth2Client } from 'google-auth-library';
 import { google } from 'googleapis';
 import { NextResponse } from 'next/server';
 
@@ -7,11 +6,11 @@ const getGoogleAuthClient = (tokens: {
   access_token: string;
   refresh_token?: string;
 }) => {
-  const oauth2Client = new OAuth2Client({
-    clientId: process.env.GOOGLE_CLIENT_ID,
-    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    redirectUri: process.env.GOOGLE_REDIRECT_URI,
-  });
+  const oauth2Client = new google.auth.OAuth2(
+    process.env.GOOGLE_CLIENT_ID,
+    process.env.GOOGLE_CLIENT_SECRET,
+    process.env.GOOGLE_REDIRECT_URI
+  );
 
   oauth2Client.setCredentials(tokens);
   return oauth2Client;
